a653f1c399337e9e7176c89c27f776a9eef69cbb
camilo-mancipe <44626685+keymiro@users.noreply.github.com>
2021-12-14 08:01:55 -0500

    Update galeria.blade.php

--- resources/views/galeria.blade.php
+++ resources/views/galeria.blade.php
@@ -21,7 +21,7 @@
                   @if($i->category->title=='galeria')
                   <div class="col-lg-3 col-md-4">
                       <div class="gallery-item">
-                          <a href="" class="galelry-lightbox">
+                          <a href="{{Storage::url($i->url)}}" class="galelry-lightbox">
                               <img src="{{Storage::url($i->url)}}" alt="" class="img-fluid">
                           </a>
                           </div>
