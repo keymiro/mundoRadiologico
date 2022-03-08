e8c79361af6ee0f466dd243061efb1bb133e3449
camilo-mancipe <44626685+keymiro@users.noreply.github.com>
2022-03-08 15:36:41 -0500

    Update navtab.blade.php

--- resources/views/layouts/navtab.blade.php
+++ resources/views/layouts/navtab.blade.php
@@ -1,50 +1,55 @@
-
-    <div class="container py-4">
-        <div class="row">
-            <div class="col-md-3">
-                <!-- Tabs nav -->
-                <div class="nav flex-column nav-pills nav-pills-custom" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                    <a class="nav-link mb-3 p-3 shadow active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">
-                        <i class="fa fa-user-circle-o mr-2"></i>
-                        <span class="font-weight-bold small text-uppercase">Personal information</span></a>
-
-                    <a class="nav-link mb-3 p-3 shadow" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">
-                        <i class="fa fa-calendar-minus-o mr-2"></i>
-                        <span class="font-weight-bold small text-uppercase">Bookings</span></a>
-
-                    <a class="nav-link mb-3 p-3 shadow" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">
-                        <i class="fa fa-star mr-2"></i>
-                        <span class="font-weight-bold small text-uppercase">Reviews</span></a>
-
-                    <a class="nav-link mb-3 p-3 shadow" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">
-                        <i class="fa fa-check mr-2"></i>
-                        <span class="font-weight-bold small text-uppercase">Confirm booking</span></a>
-                    </div>
+@php
+  $e=1;
+  $f=1;
+@endphp
+<div class="container py-4">
+    <div class="row">
+        <div class="col-md-3">
+    <!-- Tabs nav -->
+            <div class="nav flex-column nav-pills nav-pills-custom" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                @foreach ($info as $mg)
+                    @if($mg->category->title=='services')
+                        @if($f==1)
+                        <a class="nav-link mb-3 p-3 shadow active" id="v-pills-home-tab" data-toggle="pill" href="#a{{$mg->id}}" role="tab" aria-controls="v-pills-home" aria-selected="true">
+                            <span class="font-weight-bold small text-uppercase">{{$mg->title}}</span>
+                        </a>
+                        @endif
+                        @if($f>1)
+                        <a class="nav-link mb-3 p-3 shadow " id="v-pills-home-tab" data-toggle="pill" href="#a{{$mg->id}}" role="tab" aria-controls="v-pills-home" aria-selected="true">
+                            <span class="font-weight-bold small text-uppercase">{{$mg->title}}</span>
+                        </a>
+                        @endif
+                        @php
+                            $f=$mg->id;
+                        @endphp
+                    @endif
+                @endforeach
             </div>
-            <div class="col-md-9">
-                <!-- Tabs content -->
-                <div class="tab-content" id="v-pills-tabContent">
-                    <div class="tab-pane fade shadow rounded bg-white show active p-5" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-                        <h4 class="font-italic mb-4">Personal information</h4>
-                        <p class="font-italic text-muted mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-
-                    <div class="tab-pane fade shadow rounded bg-white p-5" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
-                        <h4 class="font-italic mb-4">Bookings</h4>
-                        <p class="font-italic text-muted mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-
-                    <div class="tab-pane fade shadow rounded bg-white p-5" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
-                        <h4 class="font-italic mb-4">Reviews</h4>
-                        <p class="font-italic text-muted mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-
-                    <div class="tab-pane fade shadow rounded bg-white p-5" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
-                        <h4 class="font-italic mb-4">Confirm booking</h4>
-                        <p class="font-italic text-muted mb-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-                </div>
+        </div>
+        <div class="col-md-9">
+    <!-- Tabs content -->
+            <div class="tab-content" id="v-pills-tabContent">
+                @foreach ($info as $mgs)
+                    @if($mgs->category->title=='services')
+                        @if($e==1)
+                            <div class="tab-pane fade shadow rounded bg-white show active p-5" id="a{{$mgs->id}}" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                                <h4 class="font-italic mb-4">{{$mgs->title}}</h4>
+                                <p class="font-italic text-muted mb-2">{!!$mgs->descriptionck!!}</p>
+                            </div>
+                        @endif
+                        @if($e>1)
+                            <div class="tab-pane fade shadow rounded bg-white p-5" id="a{{$mgs->id}}" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                                <h4 class="font-italic mb-4">{{$mgs->title}}</h4>
+                                <p class="font-italic text-muted mb-2">{!!$mgs->descriptionck!!}</p>
+                            </div>
+                        @endif
+                        @php
+                            $e=$mg->id;
+                        @endphp
+                    @endif
+                @endforeach
             </div>
         </div>
     </div>
+</div>
 
