d4f2cf0c30e322af4a4347771c2a3cbafbb2a990
camilo-mancipe <44626685+keymiro@users.noreply.github.com>
2022-03-08 16:17:15 -0500

    Update template.blade.php

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>Mundo Radiológico | mundoradiologico.com </title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
